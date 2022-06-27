3b4540b716024435dd188aa3bf6884b8250892bb
Thomas Jansen <thomas@ronmix.com>
2022-06-27 20:30:47 +0200

    Made a border and made the code cleaner

--- Python Informatica/Chess/Chess.py
+++ Python Informatica/Chess/Chess.py
@@ -1,6 +1,7 @@
 #Thomas Jansen
 #thomas@ronmix.com
 
+from cmath import rect
 import pygame
 import time
 import os
@@ -16,28 +17,34 @@ FPS = 60
 
 black = (0, 0, 0)
 white = (255, 255, 255)
+tile_black = (35, 28, 19)
+tile_white = (226, 191, 128)
 bg_grey = (40, 36, 44)
 
 
 board = pygame.Surface((cellSize * 8, cellSize * 8))
+pygame.Surface.fill(board, tile_black)
+
+win.fill(bg_grey)
+pygame.draw.rect(win, black, pygame.Rect(75, 5, cellSize * 8 + 10, cellSize * 8 + 10))
+
 #Function for drawing the chess board
 def draw_board():
     #loop for moving from row to row
     for x in range (0, 8, 2):
         #loop for the moving in the row
         for y in range (0, 8, 2):
-            pygame.draw.rect(board, white, (x*cellSize, y*cellSize, 80, 80))
+            pygame.draw.rect(board, tile_white, (x*cellSize, y*cellSize, 80, 80))
     
     for x in range (1, 8, 2):
         #loop for the moving in the row
         for y in range (1, 8, 2):
-            pygame.draw.rect(board, white, (x*cellSize, y*cellSize, 80, 80))
+            pygame.draw.rect(board, tile_white, (x*cellSize, y*cellSize, 80, 80))
 
 
 def draw_window():
-    win.fill(bg_grey)
     win.blit(board, (80, 10))
-    
+
     pygame.display.update()
 
 def main():
