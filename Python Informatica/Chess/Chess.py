1650d23889e7fd33e768f2df220873b832e5b7c9
Thomas Jansen <thomas@ronmix.com>
2022-06-29 15:18:01 +0200

    Update more update moreeeee

--- Python Informatica/Chess/Chess.py
+++ Python Informatica/Chess/Chess.py
@@ -46,7 +46,6 @@ list_maker()
 #variables
 cellSize = 80
 FPS = 60
-p = 0
 lst = []
 First_Bischop_B = 0
 x = 0
@@ -74,9 +73,6 @@ pygame.Surface.fill(board, tile_black)
 #Colors the background
 win.fill(bg_grey)
 
-#A nice border around the board
-pygame.draw.rect(win, black, pygame.Rect(73, 3, cellSize * 8 + 14, cellSize * 8 + 14))
-
 
 #Function for drawing the chess board
 def draw_board():
@@ -90,10 +86,14 @@ def draw_board():
         #loop for the moving in the row
         for y in range (1, 8, 2):
             pygame.draw.rect(board, tile_white, (x*cellSize, y*cellSize, 80, 80))
+    #A nice border around the board
+    pygame.draw.rect(win, black, pygame.Rect(73, 3, cellSize * 8 + 14, cellSize * 8 + 14))
     win.blit(board, (80, 10))
 
 
 def Bishop_B_Clicked ():
+    global First_Bischop_B, clicked_square_x, clicked_square_y
+    draw_board()
     k = 0
     g = 0
     ClickedX = 0
@@ -113,7 +113,7 @@ def Bishop_B_Clicked ():
             print (coordsX[clicked_square_x])
     
     while ClickedY == 0:
-        if coordsY[g] < x:
+        if coordsY[g] < y:
             g += 1
         else:
             ClickedY = 1
@@ -124,7 +124,7 @@ def Bishop_B_Clicked ():
             else:
                 clicked_square_y = g
             print (coordsY[clicked_square_y])
-    win.blit(Bischop_B, (coordsX[clicked_square_x],coordsY[clicked_square_y]))
+    First_Bischop_B = 2
     
     
 
@@ -135,19 +135,21 @@ def Bishop_B_Clicked ():
 
 #function for displaying everything
 def draw_window():
-    global First_Bischop_B
-    
+    global First_Bischop_B, clicked_square_x, clicked_square_y
+
     if First_Bischop_B == 0:
-        lst.append(win.blit(Bischop_B, (coords[p])))
+        lst.append(win.blit(Bischop_B, (coords[2])))
         First_Bischop_B = 1
-    lst[0] = win.blit(Bischop_B, (coords[p]))
+    
+    if First_Bischop_B == 2:
+        lst[0] = win.blit(Bischop_B, (coordsX[clicked_square_x],coordsY[clicked_square_y]))
+        First_Bischop_B = 1
+    
 
     pygame.display.update()
 
 def main():
     global p, x, y, aantalKliks
-
-    #Clickerd_Bishop_B = 0
 
     #makes the game run at 60 fps so it doesnt use too many computer resources
     clock = pygame.time.Clock()
@@ -166,7 +168,6 @@ def main():
                     aantalKliks += 1
                 elif aantalKliks == 1:
                     aantalKliks = 0
-                    print("2e")
                     Bishop_B_Clicked()
                             
 
